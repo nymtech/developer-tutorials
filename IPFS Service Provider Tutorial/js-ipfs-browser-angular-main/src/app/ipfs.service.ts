3deeac9004e70c7b2e4700759d2c8170bc27d07d
joeiacono2021 <90098521+joeiacono2021@users.noreply.github.com>
2023-02-09 13:16:20 +0000

    Pre merge for IPFS uploader

---
+++ IPFS Service Provider Tutorial/js-ipfs-browser-angular-main/src/app/ipfs.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+
+import { IPFS, create } from 'ipfs-core';
+import * as IPFS_ROOT_TYPES from 'ipfs-core-types/src/root';
+import { BehaviorSubject, } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class IpfsService {
+  private _ipfsSource = new BehaviorSubject<null | IPFS>(null);
+  private _createIPFSNodePromise: Promise<IPFS>;
+
+  private get ipfs() {
+    const getter = async () => {
+      let node = this._ipfsSource.getValue();
+
+      if (node == null) {
+        console.log("Waiting node creation...")
+
+        node = await this._createIPFSNodePromise as IPFS
+        this._ipfsSource.next(node);
+      }
+
+      return node;
+    }
+
+    return getter();
+  }
+
+  constructor() {
+    console.log("Starting new node...")
+
+    this._createIPFSNodePromise = create()
+  }
+
+  /**
+   * @description Get the ID information about the current IPFS node
+   * @return {Promise<IPFS_ROOT_TYPES.IDResult>}
+   */
+  async getId(): Promise<IPFS_ROOT_TYPES.IDResult> {
+    const node = await this.ipfs;
+    return await node.id();
+  }
+
+  /**
+   * @description Get the version information about the current IPFS node
+   * @return {Promise<IPFS_ROOT_TYPES.VersionResult>}
+   */
+  async getVersion(): Promise<IPFS_ROOT_TYPES.VersionResult> {
+    const node = await this.ipfs;
+    return await node.version();
+  }
+
+  /**
+   * @description Get the status of the current IPFS node
+   * @returns {Promise<boolean>}
+   */
+  async getStatus(): Promise<boolean> {
+    const node = await this.ipfs;
+    return await node.isOnline();
+  }
+
+  async addFile(object : any,uploadMethod: string): Promise<any> {
+    const node = await this.ipfs;
+    return await node.add(object);
+  }
+}
